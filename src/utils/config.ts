823406edbbe92522152a9176f1140101077127a2
Claudiney Veloso <claudineyveloso@gmail.com>
2024-08-23 01:18:39 -0300

    server soldim-api

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://soldim-api-a62e9b25ff95.herokuapp.com"
+    ? "https://soldim-api-344942c665db.herokuapp.com"
     : "http://localhost:8080";
 
 export default baseURL;
