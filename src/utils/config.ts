6b65d7f473d3c7f0d9c69fe061ccc8b4c19a7738
Claudiney Veloso <claudineyveloso@gmail.com>
2024-09-20 21:33:43 -0300

    accessing AWS EC2 port

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://soldim-api-344942c665db.herokuapp.com"
-    : "http://localhost:8080";
+  process.env.NODE_ENV === 'production'
+    ? 'http://3.80.249.138:8888'
+    : 'http://localhost:8080';
 
 export default baseURL;
