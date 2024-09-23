81b8f38facec3350d41d53db9589cac36896cbab
Claudiney Veloso <claudineyveloso@gmail.com>
2024-09-23 00:33:31 -0300

    update config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'http://3.80.249.138:8888'
+    ? 'http://54.237.168.87:8888'
     : 'http://localhost:8080';
 
 export default baseURL;
