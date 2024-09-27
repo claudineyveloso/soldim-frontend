79dcd626b16d06d8476f453cbaff8142a6668b60
Claudiney Veloso <claudineyveloso@gmail.com>
2024-09-27 16:38:32 -0300

    changed url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Somente rotas que começam com /api
+        destination: 'http://18.208.175.110:8888/:path*', // Proxy para sua API HTTP
+      },
+    ];
+  },
+};
 
 export default nextConfig;
